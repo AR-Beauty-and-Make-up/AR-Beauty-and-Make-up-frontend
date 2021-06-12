d61811c0461512429470f2644d3f2ba295b4330c
Luciana <luliialonso@hotmail.com>
2021-06-12 10:42:43 -0300

    Modifico editar usuario

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import {makeStyles} from '@material-ui/core/styles';
 import UserService from '../../services/UserService'
 import {useContext} from 'react'
-import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
